58d7540f1fd56e4b2813e0dfa66d7594344c4cfe
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2023-07-28 00:11:37 +0530

    Create heart_5.cpp

---
+++ heart_5.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+
+int main(){
+  std::cout << "What comes out of your mouth is what stored in your heart.\n";
+  std::cout << "A human heart can rest for upto a brief period of time depending on the stimuli.\n";
+  std::cout << "Did you know these heart facts?\n";  
+  return 0;
+}
