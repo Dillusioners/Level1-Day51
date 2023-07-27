6205ed18e19af8c05b731cc1bf00a3d313eb157a
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2023-07-28 00:09:21 +0530

    Create heart_4.cpp

---
+++ heart_4.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+
+int main(){
+  std::cout << "Which anime character has the warmest heart?\n";
+  std::cout << "Akeno from Highschool DxD.\n";
+  std::cout << "Very funny joke, right?\n";  
+  return 0;
+}
