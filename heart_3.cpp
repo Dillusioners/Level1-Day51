b88ae278c7e7be49f8de557afd384226a6038128
LeeTuah <77256247+LeeTuah@users.noreply.github.com>
2023-07-28 00:07:39 +0530

    Create heart_3.cpp

---
+++ heart_3.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+
+int main(){
+  std::cout << "DO you know who has the coldest heart in the world?\n";
+  std::cout << "The heart of a penguin.\n";
+  std::cout << "NOW START LAUGHING.\n";  
+  return 0;
+}
